d4ecae4c6c167fc37fc69fbddc41b89cb4824c4f
masnoble <jnthomas522@gmail.com>
2023-04-11 15:47:23 -0400

    eslint fixes

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,12 +1,12 @@
-import { NextResponse } from 'next/server';
-import prisma from '@/lib/prisma'
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
-const res = NextResponse
+const res = NextResponse;
 
 
 export async function GET(req: Request, {params}: {params:{id:string}}) {
-  const id = params.id
-  const user = await prisma.user.findFirst({ where: { id } })
-  return res.json(user)
+  const id = params.id;
+  const user = await prisma.user.findFirst({ where: { id } });
+  return res.json(user);
 }
 
